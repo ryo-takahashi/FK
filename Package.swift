f15326992e49d7d8eb7c616da882e66eb1827fb3
ryo-takahashi <10174938+ryo-takahashi@users.noreply.github.com>
2026-03-22 13:45:23 +0900

    Release 0.3.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FilterKit",
-            url: "https://github.com/ryo-takahashi/FK/releases/download/0.2.0/FilterKit.xcframework.zip",
-            checksum: "0b1d16706041d6f54a5b0abefda74aa780a2f77805579b6cbfa4ecb77f34a4bf"
+            url: "https://github.com/ryo-takahashi/FK/releases/download/0.3.0/FilterKit.xcframework.zip",
+            checksum: "22ba0c9b348f91ccb9225c7f4220d8d98b53e6f17b5a1a62a66292b341a687c7"
         )
     ]
 )
